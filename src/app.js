17a4e6b618ac82e99a9d62485795e3b87ff19ecc
vitor <vitorrcosta96@gmail.com>
2020-05-16 00:40:14 -0300

    [ADD] Cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+var cors = require('cors')
 require('./db/mongoose')
 const userRouter = require('./routers/entry')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 
