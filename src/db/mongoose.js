14ddf39b171201c26f84dba6dbd97170016b10a2
vitor <vitorrcosta96@gmail.com>
2020-05-22 19:18:39 -0300

    [ADD] Default database path

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const connectionURL = process.env.MONGODB_URL
+const connectionURL = process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/cristal-api"
 
 mongoose.connect(connectionURL, {
     useNewUrlParser: true,
